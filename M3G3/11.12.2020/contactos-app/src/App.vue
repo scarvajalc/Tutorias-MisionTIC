b7c68ae375bfa1abc03d432660bcab788d88b415
scarvajalc <santicarccc@gmail.com>
2020-12-13 19:42:10 -0500

    code

---
+++ M3G3/11.12.2020/contactos-app/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <p>{{ mensaje }}</p>
+    <button v-on:click="cambiarMensaje">Cambiar el mensaje</button>
+    <VerContactos />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import VerContactos from "./components/VerContactos";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    VerContactos
+  },
+  data: function() {
+    return {
+      mensaje: "Hola Mundo",
+      lista: ["a", "b", "c"]
+    };
+  },
+  methods: {
+    cambiarMensaje: function() {
+      this.mensaje = "Chao Mundo";
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ M3G3/11.12.2020/contactos-app/src/components/VerContactos.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>Resumen de sus contactos</h2>
+    <table>
+      <tr>
+        <th>Id del contacto</th>
+        <th>nombre del contacto</th>
+      </tr>
+      <tr v-for="contacto in lista_contactos_resumida">
+        <td>{{ contacto.id }}</td>
+        <td>{{ contacto.nombre }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VerContactos",
+  data: function() {
+    return {
+      lista_contactos_resumida: [
+        {
+          id: 1,
+          nombre: "Pepe"
+        },
+        { id: 2, nombre: "Maria" }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
